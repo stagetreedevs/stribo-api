1ecdea8926b3065d9d509f9e38f4bb9f4210b1ab
Outro-Lucas <lucasferreirapacoti@gmail.com>
2024-02-07 10:52:16 -0300

    feat: health point

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,6 +20,11 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Get('health')
+  getHealth(): string {
+    return 'OK';
+  }
+
   // @Get('login-google')
   // @ApiOperation({ summary: 'LOGIN/CADASTRO GOOGLE WEB' })
   // @UseGuards(AuthGuard('google'))
