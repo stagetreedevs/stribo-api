e04b1238508c2522d743fde1ab6e1f76196dad7b
Anderson Viana <anderson11viana@gmail.com>
2024-09-16 10:36:39 -0300

    update

--- src/controllers/property/property.service.ts
+++ src/controllers/property/property.service.ts
@@ -1,5 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { HttpException, HttpStatus, Inject, Injectable, forwardRef } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  forwardRef,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Property } from './property.entity';
@@ -7,113 +13,149 @@ import { UserService } from '../user/user.service';
 import { AdminService } from '../admin/admin.service';
 @Injectable()
 export class PropertyService {
-    constructor(
-        @InjectRepository(Property) private readonly propertyRep: Repository<Property>,
-        @Inject(forwardRef(() => AdminService)) private readonly adminService: AdminService,
-        @Inject(forwardRef(() => UserService)) private readonly userService: UserService,
-    ) { }
+  constructor(
+    @InjectRepository(Property)
+    private readonly propertyRep: Repository<Property>,
+    @Inject(forwardRef(() => AdminService))
+    private readonly adminService: AdminService,
+    @Inject(forwardRef(() => UserService))
+    private readonly userService: UserService,
+  ) {}
 
-    async create(body: Property): Promise<Property> {
-        const verify = await this.userService.findOne(body.owner);
-        if (!verify) {
-            throw new HttpException('Usuario nao encontrado', HttpStatus.BAD_REQUEST);
-        }
-        return await this.propertyRep.save(body);
+  async create(body: Property): Promise<Property> {
+    const verify = await this.userService.findOne(body.owner);
+    if (!verify) {
+      throw new HttpException('Usuario nao encontrado', HttpStatus.BAD_REQUEST);
+    }
+    return await this.propertyRep.save(body);
+  }
+
+  async findAll(): Promise<Property[]> {
+    return this.propertyRep.find();
+  }
+
+  async findAllUserProperties(id: string): Promise<Property[]> {
+    return this.propertyRep.find({ where: { owner: id } });
+  }
+
+  async findByAdmin(adminId: string): Promise<Property[]> {
+    const properties = await this.propertyRep.find();
+    const filteredProperties = properties.filter((property) =>
+      property.admins.includes(adminId),
+    );
+    return filteredProperties;
+  }
+
+  async findOne(id: string): Promise<Property> {
+    const verify = await this.propertyRep.findOne({ where: { id } });
+    if (!verify) {
+      throw new HttpException(
+        'Propriedade não encontrada',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return verify;
+  }
+
+  async findAllNames() {
+    const properties = await this.propertyRep.find({
+      select: {
+        name: true,
+        id: true,
+      },
+    });
+
+    return properties.map((property) => ({
+      label: property.name,
+      value: property.id,
+    }));
+  }
+
+  async findAdminsForProperty(propertyId: string): Promise<any[]> {
+    const property: Property = await this.findOne(propertyId);
+
+    const adminIds: string[] = property.admins;
+    const adminResults: any[] = [];
+
+    for (const adminId of adminIds) {
+      const adminResult = await this.adminService.findOne(adminId);
+      adminResults.push(adminResult);
     }
 
-    async findAll(): Promise<Property[]> {
-        return this.propertyRep.find();
+    return adminResults;
+  }
+
+  async updateInputs(id: string, body: any): Promise<Property> {
+    const verify = await this.findOne(id);
+
+    if (!verify) {
+      throw new HttpException(
+        'Propriedade nao encontrada',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    async findAllUserProperties(id: string): Promise<Property[]> {
-        return this.propertyRep.find({ where: { owner: id } });
+    verify.name = body.name || verify.name;
+    verify.code = body.code || verify.code;
+
+    await this.propertyRep.update(id, verify);
+    return this.findOne(id);
+  }
+
+  async addAdmins(id: string, adminId: string): Promise<Property> {
+    const property = await this.findOne(id);
+
+    if (!adminId) {
+      throw new HttpException(
+        'ID do administrador não fornecido',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    async findByAdmin(adminId: string): Promise<Property[]> {
-        const properties = await this.propertyRep.find();
-        const filteredProperties = properties.filter(property => property.admins.includes(adminId));
-        return filteredProperties;
+    const isAlreadyInserted = property.admins.includes(adminId);
+
+    if (isAlreadyInserted) {
+      throw new HttpException(
+        'Administrador já cadastrado na propriedade',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    async findOne(id: string): Promise<Property> {
-        const verify = await this.propertyRep.findOne({ where: { id } });
-        if (!verify) {
-            throw new HttpException('Propriedade não encontrada', HttpStatus.BAD_REQUEST);
-        }
-        return verify;
+    property.admins.push(adminId);
+
+    await this.propertyRep.update(id, { admins: property.admins });
+
+    return await this.findOne(id);
+  }
+
+  async removeAdmin(id: string, adminId: string): Promise<Property> {
+    const property = await this.findOne(id);
+
+    if (!adminId) {
+      throw new HttpException(
+        'ID do administrador não fornecido',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    async findAdminsForProperty(propertyId: string): Promise<any[]> {
-        const property: Property = await this.findOne(propertyId);
+    const index = property.admins.indexOf(adminId);
 
-        const adminIds: string[] = property.admins;
-        const adminResults: any[] = [];
-
-        for (const adminId of adminIds) {
-            const adminResult = await this.adminService.findOne(adminId);
-            adminResults.push(adminResult);
-        }
-
-        return adminResults;
+    if (index === -1) {
+      throw new HttpException(
+        'Administrador não associado à propriedade',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    async updateInputs(id: string, body: any): Promise<Property> {
-        const verify = await this.findOne(id);
+    property.admins.splice(index, 1);
 
-        if (!verify) {
-            throw new HttpException('Propriedade nao encontrada', HttpStatus.BAD_REQUEST);
-        }
+    await this.propertyRep.update(id, { admins: property.admins });
 
-        verify.name = body.name || verify.name;
-        verify.code = body.code || verify.code;
+    return await this.findOne(id);
+  }
 
-        await this.propertyRep.update(id, verify);
-        return this.findOne(id);
-    }
-
-    async addAdmins(id: string, adminId: string): Promise<Property> {
-        const property = await this.findOne(id);
-
-        if (!adminId) {
-            throw new HttpException('ID do administrador não fornecido', HttpStatus.BAD_REQUEST);
-        }
-
-        const isAlreadyInserted = property.admins.includes(adminId);
-
-        if (isAlreadyInserted) {
-            throw new HttpException('Administrador já cadastrado na propriedade', HttpStatus.BAD_REQUEST);
-        }
-
-        property.admins.push(adminId);
-
-        await this.propertyRep.update(id, { admins: property.admins });
-
-        return await this.findOne(id);
-    }
-
-    async removeAdmin(id: string, adminId: string): Promise<Property> {
-        const property = await this.findOne(id);
-
-        if (!adminId) {
-            throw new HttpException('ID do administrador não fornecido', HttpStatus.BAD_REQUEST);
-        }
-
-        const index = property.admins.indexOf(adminId);
-
-        if (index === -1) {
-            throw new HttpException('Administrador não associado à propriedade', HttpStatus.BAD_REQUEST);
-        }
-
-        property.admins.splice(index, 1);
-
-        await this.propertyRep.update(id, { admins: property.admins });
-
-        return await this.findOne(id);
-    }
-
-    async remove(id: string): Promise<void> {
-        await this.findOne(id);
-        await this.propertyRep.delete(id);
-    }
-
+  async remove(id: string): Promise<void> {
+    await this.findOne(id);
+    await this.propertyRep.delete(id);
+  }
 }
