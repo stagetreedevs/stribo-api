61828fd3ee6507c3e132dfce696a3ada1a339331
Anderson Viana <anderson11viana@gmail.com>
2025-04-08 10:37:57 -0300

    Refactor properties module to include MovementsQueryDTO and ProductsQueryDTO for filtering movements and products

--- src/controllers/properties/properties.controller.ts
+++ src/controllers/properties/properties.controller.ts
@@ -9,7 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { PropertiesService } from './properties.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { ProductsDTO, ProductsQueryDTO } from './dto/products.dto';
 import { MovementsDTO, MovementsQueryDTO } from './dto/movements.dto';
 
@@ -20,12 +20,14 @@ export class PropertiesController {
 
   @Post('products')
   @ApiOperation({ summary: 'Create a new product' })
+  @ApiBody({ type: ProductsDTO })
   async createProduct(@Body() data: ProductsDTO) {
     return await this.propertiesService.createProduct(data);
   }
 
   @Get('products')
   @ApiOperation({ summary: 'Get all products' })
+  @ApiQuery({ type: ProductsQueryDTO })
   async getAllProducts(@Query() query: ProductsQueryDTO) {
     return await this.propertiesService.getAllProducts(query);
   }
@@ -44,6 +46,7 @@ export class PropertiesController {
 
   @Put('products/:id')
   @ApiOperation({ summary: 'Update a product by ID' })
+  @ApiBody({ type: ProductsDTO })
   async updateProduct(@Param('id') id: string, @Body() data: ProductsDTO) {
     return await this.propertiesService.updateProduct(id, data);
   }
@@ -56,12 +59,14 @@ export class PropertiesController {
 
   @Post('movements')
   @ApiOperation({ summary: 'Create a new movement' })
+  @ApiBody({ type: MovementsDTO })
   async createMovement(@Body() data: MovementsDTO) {
     return await this.propertiesService.createMovement(data);
   }
 
   @Get('movements')
   @ApiOperation({ summary: 'Get all movements' })
+  @ApiQuery({ type: MovementsQueryDTO })
   async getAllMovements(@Query() query: MovementsQueryDTO) {
     return await this.propertiesService.getAllMovements(query);
   }
@@ -74,6 +79,7 @@ export class PropertiesController {
 
   @Put('movements/:id')
   @ApiOperation({ summary: 'Update a movement by ID' })
+  @ApiBody({ type: MovementsDTO })
   async updateMovement(@Param('id') id: string, @Body() data: MovementsDTO) {
     return await this.propertiesService.updateMovement(id, data);
   }
